038fe09e9d9c40c8b00639a3412c3e97d30231b4
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-12-02 22:12:55 +0700

    feat: update code

--- model/dataPelanggaran.php
+++ model/dataPelanggaran.php
@@ -1,81 +1,61 @@
 <?php
+require_once 'controller/connection.php';
+
 class DataPelanggaran {
     private $conn;
-    private $table_name = 'datapelanggaran';
 
-    public $id_pelanggaran;
-    public $id_mahasiswa;
-    public $deskripsi_pelanggaran;
-    public $tingkat_pelanggaran;
-
-    public function __construct($database_connection) {
-        $this->conn = $database_connection;
+    public function __construct() {
+        $db = new Database();
+        $this->conn = $db->getConnection();
     }
 
-    public function create() {
-        $query = "INSERT INTO " . $this->table_name . " 
-                  (id_mahasiswa, deskripsi_pelanggaran, tingkat_pelanggaran) 
-                  VALUES (?, ?, ?)";
-        
-        $this->id_mahasiswa = htmlspecialchars(strip_tags($this->id_mahasiswa));
-        $this->deskripsi_pelanggaran = htmlspecialchars(strip_tags($this->deskripsi_pelanggaran));
-        $this->tingkat_pelanggaran = htmlspecialchars(strip_tags($this->tingkat_pelanggaran));
-        
-        $params = array(
-            $this->id_mahasiswa,
-            $this->deskripsi_pelanggaran,
-            $this->tingkat_pelanggaran
-        );
-        
-        $stmt = sqlsrv_query($this->conn, $query, $params);
-        
-        return $stmt !== false;
-    }
-
-    public function read() {
+    public function getAllPelanggaran() {
         $query = "SELECT dp.*, m.nama_mahasiswa 
-                  FROM " . $this->table_name . " dp
+                  FROM datapelanggaran dp
                   LEFT JOIN mahasiswa m ON dp.id_mahasiswa = m.id_mahasiswa";
-        
-        $stmt = sqlsrv_query($this->conn, $query);
-        
-        if ($stmt === false) {
-            return false;
-        }
-        
-        return $stmt;
+        $stmt = $this->conn->query($query);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function update() {
-        $query = "UPDATE " . $this->table_name . "
-                 SET id_mahasiswa = ?, 
-                     deskripsi_pelanggaran = ?, 
-                     tingkat_pelanggaran = ?
-                 WHERE id_pelanggaran = ?";
-
-        $this->id_mahasiswa = htmlspecialchars(strip_tags($this->id_mahasiswa));
-        $this->deskripsi_pelanggaran = htmlspecialchars(strip_tags($this->deskripsi_pelanggaran));
-        $this->tingkat_pelanggaran = htmlspecialchars(strip_tags($this->tingkat_pelanggaran));
-
-        $params = array(
-            $this->id_mahasiswa,
-            $this->deskripsi_pelanggaran,
-            $this->tingkat_pelanggaran,
-            $this->id_pelanggaran
-        );
-
-        $stmt = sqlsrv_query($this->conn, $query, $params);
-
-        return $stmt !== false;
+    public function getPelanggaranById($id) {
+        $query = "SELECT dp.*, m.nama_mahasiswa 
+                  FROM datapelanggaran dp
+                  LEFT JOIN mahasiswa m ON dp.id_mahasiswa = m.id_mahasiswa
+                  WHERE dp.id_pelanggaran = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function delete() {
-        $query = "DELETE FROM " . $this->table_name . " WHERE id_pelanggaran = ?";
-        
-        $params = array($this->id_pelanggaran);
-        
-        $stmt = sqlsrv_query($this->conn, $query, $params);
-        
-        return $stmt !== false;
+    public function createPelanggaran($data) {
+        $query = "INSERT INTO datapelanggaran (id_mahasiswa, deskripsi_pelanggaran, tingkat_pelanggaran) 
+                  VALUES (?, ?, ?)";
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute([
+            $data['id_mahasiswa'], 
+            $data['deskripsi_pelanggaran'], 
+            $data['tingkat_pelanggaran']
+        ]);
+    }
+
+    public function updatePelanggaran($id, $data) {
+        $query = "UPDATE datapelanggaran SET 
+                  id_mahasiswa = ?, 
+                  deskripsi_pelanggaran = ?, 
+                  tingkat_pelanggaran = ?
+                  WHERE id_pelanggaran = ?";
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute([
+            $data['id_mahasiswa'], 
+            $data['deskripsi_pelanggaran'], 
+            $data['tingkat_pelanggaran'], 
+            $id
+        ]);
+    }
+
+    public function deletePelanggaran($id) {
+        $query = "DELETE FROM datapelanggaran WHERE id_pelanggaran = ?";
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute([$id]);
     }
 }
