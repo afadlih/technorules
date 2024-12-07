14031a3f70bcb6bfb54c39e79716a0095101ef6b
Ahmad Fadlih Wahyu Sardana <144323171+afadlih@users.noreply.github.com>
2024-12-07 14:36:22 +0700

    Repair Connection

--- controller/connection.php
+++ controller/connection.php
@@ -1,3 +1,20 @@
 <?php
-$conn = sqlsrv_connect("", array("Database" => "sistatib", "UID" => "", "PWD" => ""));
-if (!$conn) die(print_r(sqlsrv_errors(), true));
+
+// Informasi koneksi database
+$servername = "34.170.236.243,1433";
+$username = "sqlserver";
+$password = "87654321";
+$dbname = "sistatib";
+
+// Buat koneksi menggunakan PDO
+try {
+    $conn = new PDO("sqlsrv:Server=$servername;Database=$dbname", $username, $password);
+    // Set error mode ke exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Koneksi berhasil";
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+
+// Tutup koneksi
+$conn = null;
