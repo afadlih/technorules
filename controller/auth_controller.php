2efcee6012b01e0c1af11744e5066c2078472881
Ahmad Fadlih Wahyu Sardana <144323171+afadlih@users.noreply.github.com>
2024-12-09 15:02:08 +0700

    Update login logic

--- controller/auth_controller.php
+++ controller/auth_controller.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $user = new user();
+    // Use the $conn variable from connection.php
+    $user = new User($conn); // Pass the $conn connection to the User constructor
     $user->login($username, $password);
 }
-?>
